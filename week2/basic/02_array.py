db5a2a4d55ea1fcde001fab6a1abecaf16a96f12
gunrein <gunrein620@gmail.com>
2026-03-08 01:50:53 +0900

    배열 회전

--- week2/basic/02_array.py
+++ week2/basic/02_array.py
@@ -44,11 +44,21 @@ def rotate_matrix_90(matrix):
     n = len(matrix)
     
     # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
-    pass
+    rotated  = [[0 for j in range(n)] for i in range(n)]
+    """ 리프리헨션 풀이
+    temp = []
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(0)
+        temp.append(row) """
+    
         
     # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
     # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
-    pass
+    for i in range(n):
+        for j in range(n):
+            rotated[j][n-1-i] = matrix[i][j]
     
     return rotated
 
