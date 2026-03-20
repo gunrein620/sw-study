5534702293dc691214b05b76e2b4d7e110c174ed
gunrein <gunrein620@gmail.com>
2026-03-20 16:50:18 +0900

    4-3: 그래프 기초

--- week4/basic/03_graph_basic.py
+++ week4/basic/03_graph_basic.py
@@ -41,12 +41,17 @@ def create_graph(vertices, edges, directed=False):
         그래프 딕셔너리
     """
     # TODO: 빈 그래프 초기화
-    pass
+    graph = {key: [] for key in range(vertices)}
     
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
+    for u, v in edges:
     ## 무방향 그래프면 반대 방향도 추가
-    pass
+        if not directed:
+            graph[u].append(v)
+            graph[v].append(u)
+        else:
+            graph[u].append(v)
     
     return graph
 
