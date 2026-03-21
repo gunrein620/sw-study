a371ec2ab3223c86555e84e5a5366857b5ed098d
gunrein <gunrein620@gmail.com>
2026-03-21 20:17:11 +0900

    백준: 트리만들기

--- week4/problem-solving/14244번 - 트리 만들기/app.py
+++ week4/problem-solving/14244번 - 트리 만들기/app.py
@@ -9,4 +9,16 @@
 import sys;
 
 input = sys.stdin.readline
+rs = []
 
+def make_tree(n, m):
+  for i in range(n - (m-2) - 1):
+    rs.append((i,i+1))
+  for i in range(m-2):
+    rs.append((1,n-(m-2)+i))
+  return rs
+n, m = map(int, input().split())
+make_tree(n, m)
+for a, b in rs:
+  print(a, b)
+    
--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,15 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+rs = []
+
+def make_tree(n, m):
+  for i in range(n - (m-2) - 1):
+    rs.append((i,i+1))
+  for i in range(m-2):
+    rs.append((1,n-(m-2)+i))
+  return rs
+n, m = map(int, input().split())
+make_tree(n, m)
+for a, b in rs:
+  print(a, b)
